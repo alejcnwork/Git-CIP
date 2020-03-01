cd80d7e89b544396556f1c38aa988a51b19261d5
Alejandro Cea <alejcn35@gmail.com>
2020-03-01 23:13:35 +0000

    Javadoc calculadora

---
+++ Segundo DAM/Desarrollo de Interfaces/Tema Realizacion de Pruebas/Calculadora_Javadoc/src/main/java/es/formacion/cip/alejandro/vista/MenuCalculadora.java
@@ -0,0 +1,65 @@
+package es.formacion.cip.alejandro.vista;
+
+import es.formacion.cip.alejandro.controlador.Operaciones;
+
+import java.util.Scanner;
+
+/**
+ * Esta clase es una clase donde tenemos un menu, y cada menu tiene una opcion
+ * para ser elegida, depende de la opcion haremos una suma, resta, division o
+ * multiplicacion.
+ * @author Alejandro
+ * @version 0.0.3
+ *
+ */
+public class MenuCalculadora {
+
+    /**
+     * Este metodo es el menu donde estan las diferentes opciones de operaciones de la calculadora.
+     * @since 0.0.1
+     */
+    public static void Menu() {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+        do{
+            System.out.println("+ - / * CALCULADORA * / - +");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("0. Salir");
+
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1: System.out.print("Introduce el primer número a sumar: ");
+                        Double sum1 = sc.nextDouble();
+                        System.out.print("Introduce el segundo número a sumar: ");
+                        Double sum2 = sc.nextDouble();
+                        System.out.println("Resultado: " + Operaciones.suma(sum1, sum2));
+                    break;
+                case 2: System.out.print("Introduce el primer número a restar: ");
+                        Double res1 = sc.nextDouble();
+                        System.out.print("Introduce el segundo número a restar: ");
+                        Double res2 = sc.nextDouble();
+                        System.out.println("Resultado: " + Operaciones.resta(res1, res2));
+                    break;
+                case 3: System.out.print("Introduce el primer número a multiplicar: ");
+                        Double mul1 = sc.nextDouble();
+                        System.out.print("Introduce el segundo número a multiplicar: ");
+                        Double mul2 = sc.nextDouble();
+                        System.out.println("Resultado: " + Operaciones.multiplicar(mul1, mul2));
+                    break;
+                case 4: System.out.print("Introduce el dividendo de la división: ");
+                        Double div1 = sc.nextDouble();
+                        System.out.print("Introduce el divisor de la división: ");
+                        Double div2 = sc.nextDouble();
+                        System.out.println("Resultado: " + Operaciones.dividir(div1, div2) + "\nResto: " + Operaciones.restoDivision(div1, div2));
+                    break;
+                case 0: break;
+                default: System.out.println("Opción inválida");
+            }
+        } while (opcion != 0);
+        sc.close();
+    }
+}
